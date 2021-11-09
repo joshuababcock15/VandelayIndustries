b93168cba4e4fefdb3cc91236248dccd60656bae
joshuababcock15 <joshuababcock15@gmail.com>
2021-11-09 10:01:45 -0500

    Create SectionTitle.js

---
+++ src/components/SectionTitle.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const propTypes = {
+  title: PropTypes.string,
+};
+
+const defaultProps = {
+  title: 'Vandley Industries',
+};
+
+const Title = styled.h2`
+  font-size: 4rem;
+  color: white;
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: purple;
+`;
+
+export default function SectionTitle(props) {
+  const { title } = props;
+  return (
+    <Container>
+      <TitleWrapper>
+        <Title>{title}</Title>
+      </TitleWrapper>
+    </Container>
+  );
+}
+
+SectionTitle.propTypes = propTypes;
+SectionTitle.defaultProps = defaultProps;
