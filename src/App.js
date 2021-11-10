f211e2b223a0b9afb052366f07286f81c2b28bca
joshuababcock15 <joshuababcock15@gmail.com>
2021-11-10 00:30:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import Inventory from './pages/Inventory';
 import api from './api/factories';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Machiene from './pages/Machiene';
+import Machiene from './pages/Machine';
 import EditInventory from './pages/EditInventory';
+import About from './pages/About';
 
 const Content = styled.div`
   position: relative;
@@ -20,6 +21,7 @@ const Content = styled.div`
 const App = () => {
   const [warehouseData, setWarehouseData] = useState([]);
   const [inventoryData, setInventoryData] = useState([]);
+  const [factoryData, setFactoryData] = useState([]);
 
   useEffect(() => {
     const fetchWarehouse = async () => {
@@ -61,6 +63,26 @@ const App = () => {
     fetchInventory();
   }, []);
 
+  useEffect(() => {
+    const fetchFactory = async () => {
+      try {
+        const response = await api.get('./factories');
+        // console.log(response)
+        setFactoryData(response.data);
+      } catch (err) {
+        if (err.response) {
+          // not in the 200 response rangeπ
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+    fetchFactory();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -73,28 +95,27 @@ const App = () => {
             <Route exact path="/warehouses/:warehouseId/inventoryItems">
               <Inventory data={warehouseData} />
             </Route>
-            {/* <Route exact path="/factories/:factories/machienes">
-            <Machiene data={factoryData} />
-          </Route>
-          <Route exact path="/factories/:factoryId">
-            <Factory data={factoryData} />
-          </Route> */}
+            <Route exact path="/factories/:factoryId/machines">
+              <Machiene data={factoryData} />
+            </Route>
+            <Route exact path="/factories/:factoryId">
+              <Factory data={factoryData} />
+            </Route>
             <Route exact path="/warehouses/:warehouseId">
               <Warehouse data={warehouseData} />
             </Route>
             <Route exact path="/warehouses">
               <Warehouses data={warehouseData} />
             </Route>
-            {/* <Route exact path="/factories">
-            <Factories data={factoryData} />
-          </Route> */}
-            <Route exact path="/">
-              <Home />
+            <Route exact path="/factories">
+              <Factories data={factoryData} />
             </Route>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </Router>
       </Content>
-      <Footer />
+      <Footer title="Vandelay Industries" />
     </div>
   );
 };
